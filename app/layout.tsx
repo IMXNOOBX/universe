1568099f27f79c9165bf0eb7d9599f2577a89d57
IMXNOOBX <paraelmobil403@gmail.com>
2025-01-27 19:58:57 +0000

    Added robots, sitemap to improve seo & some other minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,25 +4,26 @@ import { Inter } from "next/font/google";
 import Layout from "./components/layout";
 import Navbar from "./components/navbar";
 import Watermark from "./components/utils/Watermark";
-import Favicon from '@/app/favicon.ico'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Universe",
-  icons: [{
-    rel: "icon",
-    href: "/favicon.ico",
-    url: "/favicon.ico", // Add the missing 'url' property
-  }],
+  keywords: ['portfolio', 'projects', 'IMXNOOBX', 'IMXNOOBX Portfolio', 'noob', 'noob portfolio', 'noob projects', 'discord', 'discord bot'],
+  icons: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+      url: "/favicon.ico",
+    }
+  ],
   description: "Dive into my universe of pojects in my portfolio.",
   twitter: {
     title: "Universe",
-    card: "summary",
+    card: "summary_large_image",
     images: [
-      { url: process.env.DOMAIN + '/favicon.ico' },
-      { url: "https://opengraph.githubassets.com/1/IMXNOOBX/my-universe" },
+      { url: process.env.DOMAIN + '/assets/banner.png' },
     ]
   },
   authors: [{ name: "@IMXNOOBX", url: "https://github.com/IMXNOOBX" }],
@@ -37,9 +38,9 @@ export default function RootLayout({
     <>
       {process.env.NODE_ENV !== 'development' && (
         <Script
-            defer 
-            src="https://metrics.noob.bio/script.js" 
-            data-website-id="45dbe995-b3be-41b2-9904-0d30c35de7ab"
+          defer
+          src="https://metrics.noob.bio/script.js"
+          data-website-id="45dbe995-b3be-41b2-9904-0d30c35de7ab"
         />
       )}
       <html lang="en">
